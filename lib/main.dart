19f7532bd21fad7a59ec6775748d98bbaf4b50cb
Lester Lien <lesterlien11@gmail.com>
2024-04-23 21:58:11 -0700

    change ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-// main.dart
 import 'package:flutter/material.dart';
 import 'searchPage.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -32,24 +31,41 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Conscious Cart'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.network(
-                'https://via.placeholder.com/150'), // Placeholder for logo, replace with your asset
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const SearchPage()),
-              ),
-              child: const Text('Start being Conscious about your Cart!'),
-            ),
-          ],
+        title: Text(
+          'Conscious Cart',
+          style: TextStyle(
+            fontFamily: 'Pacifico', // Use the Pacifico font
+            fontSize: 45, // Adjust font size as needed
+            fontWeight: FontWeight.bold, // Optionally, set font weight
+            color: Theme.of(context).textTheme.headline6!.color, // Use the text color from the theme
+          ),
         ),
+        centerTitle: true, // Center the title
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch, // Stretch the column horizontally
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 20, left: 16, right: 16), // Add padding around the quote
+            child: Text(
+              '"Every product holds a story. Let your purchases tell tales of compassion, sustainability, and ethical values."',
+              textAlign: TextAlign.center, // Center the quote
+              style: TextStyle(
+                fontStyle: FontStyle.italic, // Optionally, set italic style for the quote
+                fontSize: 16, // Optionally, adjust font size for the quote
+              ),
+            ),
+          ),
+          Image.network(
+              'https://via.placeholder.com/150'), // Placeholder for logo, replace with your asset
+          ElevatedButton(
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SearchPage()),
+            ),
+            child: const Text('Start being Conscious about your Cart!'),
+          ),
+        ],
       ),
     );
   }
