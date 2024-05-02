c2d99dc41cc699df1f4420e7bf04aa152bb7bb8a
KeithanNguyen <titinugen@gmail>
2024-05-02 02:35:06 -0700

    test2

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -44,7 +44,7 @@ class newHome extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 10),
+            SizedBox(height: 90),
             Container(
               padding: const EdgeInsets.all(8),
               decoration: BoxDecoration(
